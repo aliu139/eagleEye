4a1419c056c4c9dab0a500bac195ca1ee715ebca
Austin Liu <atrix315@gmail.com>
2016-09-10 11:16:42 -0400

    chore(): adding stying

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,78 @@
 var controllers = angular.module("ctrls", ['chart.js']);
 
-controllers.controller('main.ctrl', ["$scope", function($scope){
+controllers.controller('main.ctrl', ["$scope", "$http", function($scope, $http){
 	$scope.test = "derpderp";
 
-	$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [300, 500, 100];
+	$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
 
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+  $scope.mfData = [50,50];
+  $scope.mfLabels = ["Male", "Female"];
+
+  $scope.peopleNumData = [0,0,0,0];
+  $scope.peopleNumLabels = ["0", "1", "2", "3"];
 
   $scope.increment = function(){
-  	$scope.data[1][0] = $scope.data[1][0] + 5;
-  }
+  	$scope.data[1] = $scope.data[1] + 500;
+  };
+
+  var maleFemaleRatio = function(data){
+    var initial = [0,0];
+    var reducer = function(total, value){
+      if("male" in value){
+        total[0] = total[0] + 1;
+        return total;
+      }
+      else{
+        total[1] = total[1] + 1;
+        return total;
+      }
+    };
+    var res = data.reduce(reducer, initial);
+    
+    return res;
+  };
+
+  var averageNumberOfPeople = function(data){
+    var initial = {};
+    var reducer = function(total, value){
+      var num = ("male" in value) ? value.male.toString() : value.female.toString();
+      if(total != undefined && num in total){
+        total[num]= total[num] + 1; 
+      }
+      else{
+        total[num] = 1;
+      }
+
+      return total;
+    };
+    var res = data.reduce(reducer, initial);
+
+    var keys = Object.keys(res);
+    var values = [];
+    for(k in keys){
+      values.push(res[keys[k]]);
+    }
+
+    var ans = [];
+    ans.push(keys);
+    ans.push(values);
+    return ans;
+  };
+
+  window.setInterval(function(){ 
+    $http({
+      method: 'GET',
+      url: 'https://eagleeye123.firebaseio.com/data.json'
+    }).then(function successCallback(response) {
+      // console.log(response.data);
+      $scope.mfData = maleFemaleRatio(response.data);
+      $scope.peopleNumData = averageNumberOfPeople(response.data)[1];
+      $scope.peopleNumLabels = averageNumberOfPeople(response.data)[0];
+      // $scope.data = response.data;
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+  }, 500);
+
 }]);
